898e6481368ba53787338c00afac1a035d9d1f1e
anna <anka-kudrow@mail.ru>
2025-06-16 10:35:56 +0300

    refactor and correct styles on home page

--- components/custom/productCard.tsx
+++ components/custom/productCard.tsx
@@ -1,4 +1,4 @@
-import { CarFront, Heart, Scale } from 'lucide-react';
+import { CarFront, Heart, Scale } from "lucide-react";
 import Image from "next/image";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardFooter } from "~/components/ui/card";
@@ -7,7 +7,7 @@ import type { Product } from "~/lib/types";
 function ProductCard({ product }: { product: Product }) {
   return (
     <Card className="max-w-[300px] gap-2 py-0">
-      <CardContent className="px-0 space-y-1">
+      <CardContent className="space-y-1 px-0">
         <Image
           className="rounded-t-xl"
           src={product.images.image[0]}
@@ -15,13 +15,17 @@ function ProductCard({ product }: { product: Product }) {
           height={200}
           alt={product.mark_id}
         />
-        <div className="px-4 space-y-1">
-          <h3 className='font-bold'>{`${product.mark_id} ${product.folder_id}`}
+        <div className="space-y-1 px-4">
+          <h3 className="font-bold">
+            {`${product.mark_id} ${product.folder_id}`}
           </h3>
-          <div  className='font-bold'>
+          <div className="font-bold">
             {`${product.price} ${product.currency}`}
           </div>
-          <div className='flex items-center gap-1'><CarFront className='size-4' />{product.modification_id}</div>
+          <div className="flex items-center gap-1">
+            <CarFront className="size-4" />
+            {product.modification_id}
+          </div>
           <div>
             <span>{`${product.run} км`}</span> <span>{product.gearbox}</span>
           </div>
@@ -34,8 +38,14 @@ function ProductCard({ product }: { product: Product }) {
       <CardFooter className="px-4 pb-6">
         <div className="flex w-full justify-between">
           <div className="flex gap-2">
-            <Button variant="secondary" > <Heart /></Button>
-            <Button variant="secondary" > <Scale /></Button>
+            <Button variant="secondary">
+              {" "}
+              <Heart />
+            </Button>
+            <Button variant="secondary">
+              {" "}
+              <Scale />
+            </Button>
           </div>
           <Button>КУПИТЬ</Button>
         </div>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,29 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import type { Product, ProductMeta } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export async function fetchProducts({
+  queryKey,
+}: { queryKey: [string, { page?: number; sort?: "asc" | "desc" }] }): Promise<{
+  data: Product[];
+  meta: ProductMeta;
+}> {
+  const [, { page = 1, sort = undefined } = {}] = queryKey;
+
+  const searchParams = new URLSearchParams();
+  searchParams.set("_limit", "12");
+  searchParams.set("_page", String(page));
+
+  if (sort) {
+    searchParams.set("_sort", "price");
+    searchParams.set("_order", sort);
+  }
+
+  const res = await fetch(`/api/cars?${searchParams.toString()}`);
+  const json = await res.json();
+  return { data: json.data, meta: json.meta };
 }
